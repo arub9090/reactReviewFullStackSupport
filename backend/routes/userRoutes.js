cacd8b241a5ceed531b1827de52c68684326cde6
Arif Rubayet <aruba055@uottawa.ca>
2022-04-29 12:21:45 -0400

    Till Page creation of Home, register and login

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express= require('express');
+const { route } = require('express/lib/router');
 const router= express.Router();
-const {registerUser, loginUser} = require('../controller/userController')
-
-router.post('/registeruser/', registerUser)
-router.post('/login/', loginUser)
+const {registerUser, loginUser, getMe} = require('../controller/userController')
+const {protect}= require('../middleware/authMiddleware')
+router.post('/register', registerUser)
+router.post('/login', loginUser)
+router.get('/me',protect, getMe)
 
 
 module.exports= router;
